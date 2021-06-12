13bcd091bb49c691f10ac888f50e12dd95a24227
mrouetbi <mrouetbi@anypli.com>
2021-06-12 11:08:44 +0100

    react basic setup with laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-require('./bootstrap');
+import React from 'react'
+import ReactDom from 'react-dom'
+
+ReactDom.render(
+    <React.StrictMode>
+        <h1>Hello React</h1>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
