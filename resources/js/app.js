3ed5dfd4388f5cfbed745d2301e74d9d0cb4b801
mrouetbi <mrouetbi@anypli.com>
2021-06-12 12:08:32 +0100

    install react router & material-ui core

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,25 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+import SignInPage from './pages/auth/SignInPage';
+import SignUpPage from './pages/auth/SignUpPage';
 
-ReactDom.render(
-    <React.StrictMode>
-        <h1>Hello React</h1>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/">
+                        <SignInPage/>
+                    </Route>
+                    <Route exact path="/register">
+                        <SignUpPage/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
